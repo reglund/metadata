34c5f6954f4c56c263e25ca02227fb8b4ef57f5d
Asbjørn Reglund Thorsen <asbjornt@fsat.no>
2015-11-18 19:10:10 +0100

    Rescue if 404 on a file

--- google-dork.rb
+++ google-dork.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 require 'rubygems'
 require 'google-search'
-require 'open-uri'
+#require 'open-uri'
 require 'uri'
 require 'exiftool'
 
@@ -15,6 +15,7 @@ url  = ARGV[0]
 dork = ARGV[1]
 download = ARGV[2]
 html_file = File.open("/tmp/metadata.html", "w")
+$full_uri_to_file = ""
 
 if download == "1"
   p "Ok, we'll download the files too"
@@ -22,24 +23,35 @@ if download == "1"
   
   results.each do |result|
     p result.uri
+    
     uri = URI(result.uri)
+    $full_uri_to_file = uri
     filename = URI(uri).path.split('/').last
-    download = open(uri)
-    IO.copy_stream(download, "/tmp/#{filename}")
+    begin
+      download = open(uri)
+      IO.copy_stream(download, "/tmp/#{filename}")
+    rescue OpenURI::HTTPError => e
+      puts "Can't access #{ url }"
+      puts e.message
+      puts
+      next
+    end
   end
 else
   p "Not going to download the files, only run exiftool on existing #{dork} files"
 end
 
-html_file.write("<html><title>Metadata</title><body>")
+html_file.write("<html><meta charset=\"UTF-8\"><title>Metadata</title><body>")
 
 Dir.glob("/tmp/*.#{dork}") do |file|
-  html_file.write("<table border=\"solid\"><th>Filename: <a href=\"#{file}\">#{file}:</a></th><th>Data</th>")
+  html_file.write("<br/><br/>")
+  html_file.write("<table border=\"solid\"><th>Nr</th><th>Filename: <a href=\"#{file}\">#{file}:</a></th><th>Url to original: #{$full_uri_to_file}</th>")
   e = Exiftool.new(file)
   data = e.to_hash
-  
+  counter = 1
   for key in data.keys()
-    html_file.write("<tr><td>#{key}</td><td>#{data[key]}</td></tr>")
+    html_file.write("<tr><td>#{counter}</td><td>#{key}</td><td>#{data[key]}</td></tr>")
+    counter+=1
   end
   
   html_file.write("</table>")  
